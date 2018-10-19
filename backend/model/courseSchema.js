15af6a57dc87c59e7fb93e52364d547697377edc
Fay Vor <phavorsparks@gmail.com>
2018-10-19 18:28:54 +0100

    Setup course schema

--- backend/model/courseSchema.js
+++ backend/model/courseSchema.js
@@ -5,5 +5,16 @@ const courseSchema = new Schema({
   title: { type: String, required: true },
   image_url: { type: String, required: true },
   level: { type: String, required: true },
-  short_summary: { type: String, required: true }
+  short_summary: { type: String, required: true },
+  summary: { type: String },
+  instructors: [String],
+  required_knowledge: { type: String },
+  expected_learning: { type: String, required: true },
+  available: { type: Boolean, required: true },
+  expected_duration: { type: String, required: true },
+  type: { type: String, required: true }
 })
+
+const Course = mongoose.model('Courses', courseSchema)
+
+module.exports = Course
