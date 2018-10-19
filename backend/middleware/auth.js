16bcdbccf8f5c7fc5df9ccee556fce0a72fda31a
Fay Vor <phavorsparks@gmail.com>
2018-10-19 17:25:23 +0100

    Authorize get all users

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -10,6 +10,9 @@ module.exports = (req, res, next) => {
 
     next()
   } catch (error) {
-    res.status(400).send('Invalid Token')
+    res.status(400).json({
+      message: 'Invalid Token',
+      error
+    })
   }
 }
