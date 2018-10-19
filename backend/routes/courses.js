7fe6a64ba48faa80df495cf5efaaca7bfeb8c3c1
Fay Vor <phavorsparks@gmail.com>
2018-10-19 21:26:01 +0100

    Add courses routes

---
+++ backend/routes/courses.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const router = express.Router()
+
+const courseController = require('../controllers/courses')
+const checkAuth = require('../middleware/auth')
+
+// Get all courses
+router.get()
+
+// Get a single course
+router.get()
+
+// Get All video courses
+router.get()
+
+// Get a single video
+router.get()
+
+// Get All free courses
+router.get()
+
+// Get a single free course
+router.get()
+
+// Get all Paid courses
+router.get()
+
+// Get a single paid course
+router.get()
+
+module.exports = router
